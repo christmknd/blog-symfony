be9fce9d30412108656c3609dc60d84ec6aeab95
christmknd <christmokonda@yahoo.fr>
2019-12-05 03:34:45 +0100

    creation du formulaire pour les Articles

--- blog-symfony/src/Entity/Article.php
+++ blog-symfony/src/Entity/Article.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ArticleRepository")
@@ -24,6 +25,7 @@ class Article
     private $picture;
 
     /**
+     * @Assert\NotBlank
      * @ORM\Column(type="string", length=255)
      */
     private $title;
