c2c39c1ed78226210ea4d791fc36fd9b067e6f02
christmknd <christmokonda@yahoo.fr>
2019-12-05 03:57:05 +0100

    formulaire ArticleType terminé

--- blog-symfony/src/Form/ArticleType.php
+++ blog-symfony/src/Form/ArticleType.php
@@ -3,23 +3,35 @@
 namespace App\Form;
 
 use App\Entity\Article;
+use App\Entity\Category;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+
+
 
 class ArticleType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('picture')
-            ->add('title')
-            ->add('content')
-            ->add('publicationDate')
-            ->add('lastUpdateDate')
-            ->add('isPublished')
-            ->add('categories')
-        ;
+        ->add('picture', FileType::class)
+        ->add('title', TextType::class)
+        ->add('content', TextareaType::class)
+        ->add('isPublished', CheckboxType::class)
+        ->add('categories', EntityType::class, [
+            'class' => Category::class,
+            'choice_label' => 'label',
+            'multiple' => true,
+            'expanded' => false
+        ])
+    ;
     }
 
     public function configureOptions(OptionsResolver $resolver)
